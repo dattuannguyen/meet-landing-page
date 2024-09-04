780cd312714fdf62930c67026711961975f09149
dattuannguyen <dat.nt0609@gmail.com>
2024-09-04 12:02:12 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      redHat: ["Red Hat Display", "sans-serif"],
+    },
+
+    extend: {
+      colors: {
+        cyan: {
+          600: "#4D96A9",
+          300: "#8FE3F9",
+        },
+        purple: {
+          600: "#855FB1",
+          300: "#D9B8FF",
+        },
+        slate: {
+          900: "#28283D",
+          600: "#87879D",
+          300: "#D1D1DF",
+        },
+      },
+      backgroundImage: {
+        mobile: "url('../starter-code/assets/mobile/image-footer.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
